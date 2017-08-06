7163000db01eefc0add1aa4cf01268d32d15b692
Mani Arsham <moneyfx@gmail.com>
2017-08-06 13:34:53 -0400

    is one edit away

---
+++ 5/isOneEditAway.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+*
+* @param string $str1
+* @param string $str2
+*
+* @return bool
+*/
+function isOneEditAway($str1, $str2)
+{
+	if (abs(strlen($str1) - strlen($str2)) > 1) {
+		return false;
+	}
+
+	//Swap if applies
+	if (strlen($str1) < strlen($str2)) {
+		$tmp = $str1;
+		$str1 = $str2;
+		$str2 = $tmp;
+	}
+
+	$hm = array();
+
+	for ($i = 0; $i < strlen($str1); $i++) {
+		$currentChar = $str1[$i];
+		if (array_key_exists($currentChar, $hm)) {
+			$hm[$currentChar]++;
+		} else {
+			$hm[$currentChar] = 1;
+		}
+	}
+
+	for ($i = 0; $i < strlen($str2); $i++) {
+		$currentChar = $str2[$i];
+		if (array_key_exists($currentChar, $hm)) {
+			$hm[$currentChar]--;
+		}
+	}
+
+	$numberOfEdit = 0;
+	foreach ($hm as $k => $v) {
+		if ($v > 0) {
+			$numberOfEdit++;
+		}
+	}
+
+	return ($numberOfEdit == 1);	
+}
+
+$test1 = isOneEditAway('ple', 'pale');
+var_dump($test1);
+
+$test2 = isOneEditAway('pales', 'pale');
+var_dump($test2);
+
+$test3 = isOneEditAway('pale', 'bale');
+var_dump($test3);
+
+$test4 = isOneEditAway('pale', 'bake');
+var_dump($test4);
+
+$test5 = isOneEditAway('apple', 'aple');
+var_dump($test5);
+
