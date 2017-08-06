5f7c420a900f9a7e6e2799cd493784bab5fedb8a
Mani Arsham <moneyfx@gmail.com>
2017-08-06 00:13:55 -0400

    urlfy

--- 3/urlify.php
+++ 3/urlify.php
@@ -1,7 +1,10 @@
 <?php
+/** tips:
+* It is often easier to modify strings by going from end to beginning 
+*/
 
 /**
-* Replace All spaces in a string with '%20'
+* Replaces All spaces in a string with '%20'
 *
 * @param string $str
 * @param int $len
@@ -10,13 +13,33 @@
 */
 function urlify($str, $len)
 {
-	$charArray = str_split($str);
-	$count = count($charArray);
+	$lengthOfString 		= strlen($str);
+	$locationOfLastChar 	= 0;
+	$locationOfCursor 		= $lengthOfString - 1;
 
-	for ($i = $count - 1; $i >= 0; $i-- )  {
-		$currentChar = $charArray[$i];
-
+	for ($i = 0; $i < $lengthOfString; $i++) {
+		if ($str[$i] != ' ') {
+			$locationOfLastChar = $i;
+		}
 	}
 
-	return implode($charArray);
+	for ($i = $locationOfLastChar; $i >= 0; $i--) {
+		$currentChar = $str[$i];
+		if ($currentChar != ' ') {
+			$str[$locationOfCursor] = $currentChar;
+			$locationOfCursor--;
+		} else {
+			$str[$locationOfCursor] 	= '0';
+			$str[$locationOfCursor - 1] = '2';
+			$str[$locationOfCursor - 2] = '%';
+			$locationOfCursor -= 3;
+		}
+	}
+
+	return $str;
 }
+
+$test1 = urlify("Mr John Smith    ", 13);
+////////////////"Mr%20John%20Smith"
+
+var_dump($test1);
